56ee50b034262dbfcbf9f16907172adcd6831b16
ZhumabekovAlim <145676784+ZhumabekovAlim@users.noreply.github.com>
2024-04-21 13:40:07 +0500

    Update main.py

--- nlp/main.py
+++ nlp/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import openai
+import uvicorn
 
 app = FastAPI()
-openai_api_key = "sk-proj-Qhx9pfRtzZNrqtg2YGNCT3BlbkFJ3yR3fMx5v9UwVmQeu7wr"
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
+)
+
+openai_api_key = "sk-proj-9kLlLOwSPSj6qSnfni0ET3BlbkFJWUTrdYb2XxH7mNOBxd2s"
 
 with open("sight_details.txt", "r", encoding="utf-8") as file:
     dataset_text = file.read()
@@ -15,8 +27,17 @@ class Query(BaseModel):
 
 @app.post("/query/")
 async def answer_query(query: Query):
-    return {"answer": "This is a test response"}
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4-turbo",  
+            messages=[{"role": "system", "content": context+"(дай ответ максимум в 110 слов)"}, {"role": "user", "content": query.question}],
+            max_tokens=500,
+            api_key=openai_api_key
+        )
+        answer_text = response.choices[0].message['content'].strip()
+        return {"answer": answer_text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
